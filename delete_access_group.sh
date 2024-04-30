ffdb72299e95fd2544f072ac1dd0c85c2e75b81c
AnilCM <56347169+Anil-CM@users.noreply.github.com>
2024-04-30 22:43:42 +0530

    Create delete_access_group.sh

---
+++ delete_access_group.sh
@@ -0,0 +1,9 @@
+#! bin/bash
+
+IAM_TOKEN=$TOKEN
+base_url=$base_url
+access_group_id=$access_group_id
+response=$(curl -X GET --location --header "Authorization: Bearer ${IAM_TOKEN}" --header "Accept: application/json" "${base_url}/v2/groups/${access_group_id}/members")
+members=$(echo "$response" | jq '.members[] | .iam_id')
+curl -X POST --location --header "Authorization: Bearer ${IAM_TOKEN}" --header "Accept: application/json" --header "Content-Type: application/json" --data '{ "members": [ '${members}' ]}' "${base_url}/v2/groups/${access_group_id}/members/delete"
+curl -X DELETE --location --header "Authorization: Bearer ${IAM_TOKEN}" "${base_url}/v2/groups/${access_group_id}"
