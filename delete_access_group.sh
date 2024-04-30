c09f584af601a7148070e0cd53b84e4e10731d4b
AnilCM <56347169+Anil-CM@users.noreply.github.com>
2024-04-30 22:44:30 +0530

    Update delete_access_group.sh

--- delete_access_group.sh
+++ delete_access_group.sh
@@ -1,6 +1,6 @@
 #! bin/bash
 
-IAM_TOKEN=$TOKEN
+IAM_TOKEN=$IC_IAM_TOKEN
 base_url=$base_url
 access_group_id=$access_group_id
 response=$(curl -X GET --location --header "Authorization: Bearer ${IAM_TOKEN}" --header "Accept: application/json" "${base_url}/v2/groups/${access_group_id}/members")
